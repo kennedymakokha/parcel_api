9b7bdb1c29c5d78b13d5eafcf23d469406a2bc4c
kennedymakokha <katchibo2@gmail.com>
2026-05-07 02:37:56 -0600

    fixing  the  payments v4

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -3,12 +3,7 @@ import { get_Mpesa_logs, makePayment } from "../controllers/mpesa.controller";
 
 const router = Router();
 
-
-
-
 router.post("/", makePayment);
 router.get("/", get_Mpesa_logs);
-
-
 
 export default router;
